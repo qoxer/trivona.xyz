b6e158fe6b9236786dec4cc3cb3075155cd8ebab
301 <136124448+qoxer@users.noreply.github.com>
2026-04-12 20:09:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Простой скрипт для анимаций (если нужно будет добавить)
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.button');
+    
+    button.addEventListener('click', function() {
+        console.log('Перешли на Telegram бот');
+    });
+});
